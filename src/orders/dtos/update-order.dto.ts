b9c02e82323fefd6929f8ab5ab0e88855b511e33
SuchitaSriramkaNagarro <suchita.sriramka@nagarro.com>
2022-09-08 11:43:38 +0530

    validations

--- src/orders/dtos/update-order.dto.ts
+++ src/orders/dtos/update-order.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty, IsString } from "class-validator"
+import { IsNotEmpty, IsString, MinLength } from "class-validator"
 import { ApiProperty } from "@nestjs/swagger/dist/decorators"
 
 export class UpdateOrderDto{
 
     @ApiProperty()
     @IsString()
+    @MinLength(3)
+    @IsNotEmpty()
     customerName: string
 }
