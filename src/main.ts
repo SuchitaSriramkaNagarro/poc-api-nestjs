37d0d62ed7f6f1a75ab085bc6ea8fdf7b9150951
SuchitaSriramkaNagarro <suchita.sriramka@nagarro.com>
2022-09-08 10:51:42 +0530

    setup swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import {OrdersModule} from './orders/orders.module'
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+  .setTitle('Orders')
+  .setDescription('The Order Details API description')
+  .setVersion('0.1')
+  .build();
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api', app, document)
+  
   await app.listen(8080);
 }
 bootstrap();
