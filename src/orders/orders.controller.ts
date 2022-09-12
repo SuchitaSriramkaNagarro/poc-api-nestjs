5e87c55792b0f8be317c929b558e6df43b1f3660
SuchitaSriramkaNagarro <suchita.sriramka@nagarro.com>
2022-09-12 11:01:17 +0530

    error handling

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,33 +1,40 @@
-import { Controller, Get, Body, Patch, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, UseFilters } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiBadRequestResponse, ApiGatewayTimeoutResponse } from '@nestjs/swagger';
 import { OrderEntity } from './entities/order.entity';
+import { PrismaClientExceptionFilter } from 'src/prisma-client-exception.filter';
 
 @Controller('/api/v2/orders')
 @ApiTags('orders')
+@UseFilters(PrismaClientExceptionFilter)
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Get()
   @ApiOkResponse({type: OrderEntity, isArray: true})
+  @ApiGatewayTimeoutResponse()
   findAll() {
     return this.ordersService.findAll();
   }
 
   @Get(':orderNumber')
   @ApiOkResponse({type: OrderEntity})
+  @ApiNotFoundResponse({description: 'Record with given order number not found'})
   async findOne(@Param('orderNumber') orderNumber: string) {
     const order = await this.ordersService.findOne(orderNumber);
-    if(!order){
-      throw new NotFoundException(`Could not find order with ${orderNumber}.`)
-    }
+    // if(!order){
+    //   throw new NotFoundException(`Could not find order with ${orderNumber}.`)
+    // }
     return order;
   }
 
   @Patch(':orderNumber')
   @ApiOkResponse({type: OrderEntity})
+  @ApiNotFoundResponse({description: 'Record with the given order number not found'})
+  @ApiBadRequestResponse({description: 'Bad Request Response'})
   update(@Param('orderNumber') orderNumber: string, @Body() updateOrderDto: UpdateOrderDto) {
+    
     return this.ordersService.update(orderNumber, updateOrderDto);
   }
 
