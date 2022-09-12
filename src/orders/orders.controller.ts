d85fed7fa3964274fad2eb7b374d10fe640cbb43
SuchitaSriramkaNagarro <suchita.sriramka@nagarro.com>
2022-09-12 11:15:50 +0530

    error handling @ApiResponse

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Body, Patch, Param, UseFilters } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiBadRequestResponse, ApiGatewayTimeoutResponse } from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiBadRequestResponse, ApiGatewayTimeoutResponse, ApiServiceUnavailableResponse } from '@nestjs/swagger';
 import { OrderEntity } from './entities/order.entity';
 import { PrismaClientExceptionFilter } from 'src/prisma-client-exception.filter';
 
@@ -13,7 +13,8 @@ export class OrdersController {
 
   @Get()
   @ApiOkResponse({type: OrderEntity, isArray: true})
-  @ApiGatewayTimeoutResponse()
+  @ApiGatewayTimeoutResponse({description: `Gateway timedout`})
+  @ApiServiceUnavailableResponse({description: `Service unavailable`})
   findAll() {
     return this.ordersService.findAll();
   }
@@ -21,6 +22,8 @@ export class OrdersController {
   @Get(':orderNumber')
   @ApiOkResponse({type: OrderEntity})
   @ApiNotFoundResponse({description: 'Record with given order number not found'})
+  @ApiGatewayTimeoutResponse({description: `Gateway timedout`})
+  @ApiServiceUnavailableResponse({description: `Service unavailable`})
   async findOne(@Param('orderNumber') orderNumber: string) {
     const order = await this.ordersService.findOne(orderNumber);
     // if(!order){
@@ -33,6 +36,8 @@ export class OrdersController {
   @ApiOkResponse({type: OrderEntity})
   @ApiNotFoundResponse({description: 'Record with the given order number not found'})
   @ApiBadRequestResponse({description: 'Bad Request Response'})
+  @ApiGatewayTimeoutResponse({description: `Gateway timedout`})
+  @ApiServiceUnavailableResponse({description: `Service unavailable`})
   update(@Param('orderNumber') orderNumber: string, @Body() updateOrderDto: UpdateOrderDto) {
     
     return this.ordersService.update(orderNumber, updateOrderDto);
